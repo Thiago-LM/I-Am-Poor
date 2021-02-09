16b1c564c8405de413edbdc233da1355a653804f
Thiago Leal Menezes <thiago.pixelado@gmail.com>
2021-02-09 11:07:14 -0300

    Primeiro commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+![Copper Ore icon icon by Icons8](./images/icons8-copper-ore-48.png)
+# I Am Poor
+
+A Flutter project based on The I Am Poor Challenge of [Flutter Bootcamp with Dart](https://www.udemy.com/course/flutter-bootcamp-with-dart/), by [Dr. Angela Yu](https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/).
+
+## Getting Started
+
+A few resources to get you started in this project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [Copper Ore icon](https://icons8.com/icons/set/copper-ore) icon by [Icons8](https://icons8.com)
+- [Flutter Course Resources - Section 5: I Am Poor - App Challenge](https://github.com/londonappbrewery/Flutter-Course-Resources#section-5-i-am-poor---app-challenge)
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'I Am Poor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'I Am Poor'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+        centerTitle: true,
+        backgroundColor: Colors.blue[900],
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: FittedBox(
+          child: Image.asset(
+            'images/icons8-copper-ore-48.png',
+            scale: 0.15,
+          ),
+        ),
+      ),
+    );
+  }
+}
